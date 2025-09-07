f87a7c25f626bd7533ba69657796c37d92cc8e06
Manish patil <manishpatil7058@gmail.com>
2025-09-07 18:53:39 +0530

    basic v1

--- src/main/java/com/qtodo/service/TodoItemGetService.java
+++ src/main/java/com/qtodo/service/TodoItemGetService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
