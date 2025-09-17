c319d07710a942dc114d47c0d5c0ef50a326461c
Manish Patil <manishpatilelf@proton.me>
2025-09-17 10:32:48 +0530

    Update FrontendController.java

--- src/main/java/com/qtodo/controller/FrontendController.java
+++ src/main/java/com/qtodo/controller/FrontendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FrontendController {
 
-	 @GetMapping(value = {"/ang"})
+	 @GetMapping(value = {"/ang", "/ang/"})
 	 public String forward() {
 	    return "forward:/ang/index.html";
 	 }
