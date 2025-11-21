6e0b2d122d9c030890a43a8fab108c63605a417e
Manish Patil <manishpatilelf@proton.me>
2025-11-21 11:14:07 +0530

    Create OpenApiSecurityConfig.java for jwt auth in swagger-ui

---
+++ src/main/java/com/qtodo/auth/OpenApiSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.qtodo.auth;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SecurityScheme(
+        name = "bearerAuth",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT"
+)
+@OpenAPIDefinition(
+        security = { @SecurityRequirement(name = "bearerAuth") }
+)
+public class OpenApiSecurityConfig {}
