5c46f9888bd92b9f4b00551606821c992fccf303
Manish patil <manishpatil7058@gmail.com>
2025-11-23 11:30:15 +0530

    404 ang redirect to index.html

--- src/main/java/com/qtodo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/qtodo/controller/GlobalExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.qtodo.controller;
 
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import com.qtodo.response.ApiResponseBase;
 import com.qtodo.response.ValidationException;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -29,6 +33,23 @@ public class GlobalExceptionHandler {
 //    	ex.printStackTrace();
     	logger.error("Auth Exception", ex);
         return ApiResponseBase.asWrapped(new ApiResponseBase("Unable to process request "+ex.getMessage(), HttpStatus.FORBIDDEN));
+    }
+
+    
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity handleResourceNotFound(NoResourceFoundException ex, HttpServletRequest request) {
+        logger.error("Resource not found: "+ ex.getMessage());
+        String requestUrl = request.getRequestURI();
+        
+        if (requestUrl.startsWith("/ang/")) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Location", "/ang/index.html");
+
+            // Return a 302 redirect to /ang/index.html
+            return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);  
     }
 
     @ExceptionHandler(Exception.class)
